1edcd85e153f59e29385efd9dd6ab9f73fd38965
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-24 23:21:09 +0000

    feat(tensor): implement basic Tensor class

---
+++ miyarai/__init__.py
@@ -0,0 +1,5 @@
+from .tensor import Tensor
+
+__version__ = "0.1.0"
+
+__all__ =["Tensor"]
---
+++ miyarai/tensor/tensor.py
@@ -0,0 +1,7 @@
+class Tensor:
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return f"Tensor({self.data})"
