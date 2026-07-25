9dadb2a227dd5b2745a478c9c32623ebe2541051
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-25 00:34:24 +0000

    feat(tensor): add shape inference and tensor metadata

--- miyarai/tensor/tensor.py
+++ miyarai/tensor/tensor.py
@@ -2,6 +2,29 @@ class Tensor:
 
     def __init__(self, data):
         self.data = data
+        self.shape = self._infer_shape(data)
+        self.ndim = len(self.shape)
+        self.size =self._calculate_size(self.shape)
+
+    def _infer_shape(self,data):
+        shape = []
+
+        while isinstance(data,list):
+            shape.append(len(data))
+
+            if len(data) == 0:
+                break
+            data = data[0]
+        return tuple(shape)
+    
+    def _calculate_size(self,shape):
+
+        size = 1
+
+        for dimension in shape:
+            size *= dimension
+
+        return size
 
     def __repr__(self):
         return f"Tensor({self.data})"
