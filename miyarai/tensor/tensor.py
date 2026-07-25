fee4bc6a866ec840c18f0efc3d2143cbd0056f9f
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-25 01:19:20 +0000

    feat(tensor): implement tensor item assignment

--- miyarai/tensor/tensor.py
+++ miyarai/tensor/tensor.py
@@ -54,6 +54,9 @@ class Tensor:
     def __getitem__(self,index):
         return self.data[index]
 
+    def __setitem__(self, index, value):
+        self.data[index] = value
+    
     def __len__(self):
         return len(self.data)
     
